55788d5228c7baa2bd4a3bc2b3e43ab84e556445
阿部　健一　 <c5610132@gmail.com>
2021-02-08 16:56:17 +0900

    add settings, rubocop, rspec

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,15 @@ module App
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.test_framework :rspec, 
+            request_spec: true,
+            model_spec: true,
+            view_specs: false, 
+            helper_specs: false, 
+            controller_specs: false, 
+            routing_specs: false
+    end
   end
 end
